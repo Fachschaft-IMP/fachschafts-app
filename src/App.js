4d9381f58f1504bb9ab99c0442d6671d7fbf90d2
Billamw <66533754+Billamw@users.noreply.github.com>
2023-10-15 12:05:05 +0200

    update
     followed https://react.dev/learn til Rendering lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <button type="reset">
+        I'm a button
+      </button>
+  )
 }
 
-export default App;
+function MyApp() {
+  return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+  )
+}
+const user = {
+    name: 'Johan Dasbach',
+    imageUrl: logo,
+    imageSize: 90,
+};
+function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img src={user.imageUrl}
+                 alt={'Photo of ' + user.name}
+            style={{
+                width: user.imageSize,
+                height: user.imageSize
+            }}
+            />
+        </>
+    );
+}
+
+export default Profile;
